d6c828433fc92676dfbffa8540e70facf3f8b509
Zingabopp <zingabopp@gmail.com>
2019-07-26 00:17:58 -0500

    Migrated SongFeedReaders and WebUtilities from SyncSaberService (fits better here)

---
+++ SongFeedReadersTests/MockClasses/MockTests/MockHttpResponseTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Linq;
+using System.IO;
+using SongFeedReaders;
+using Newtonsoft.Json.Linq;
+
+namespace SongFeedReadersTests.MockClasses.MockTests
+{
+    [TestClass]
+    public class MockHttpResponseTests
+    {
+        
+    }
+}

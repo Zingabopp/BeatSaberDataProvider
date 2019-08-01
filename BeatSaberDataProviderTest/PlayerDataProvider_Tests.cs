a898eb986af3225f962f1d9e0c3844e8f9d31e86
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-08-01 10:42:38 -0500

    Added test class for PlayerDataProvider (doesn't do much yet)

---
+++ BeatSaberDataProviderTest/PlayerDataProvider_Tests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BeatSaberDataProvider;
+using System.Collections;
+using System.Linq;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using BeatSaberDataProvider.DataProviders;
+using BeatSaberDataProvider.DataModels;
+using Microsoft.EntityFrameworkCore;
+using BeatSaberDataProvider.Util;
+using System.Linq.Expressions;
+using System;
+
+namespace BeatSaberDataProviderTest
+{
+    [TestClass]
+    public class PlayerDataProvider_Tests
+    {
+        [TestMethod]
+        public void ProviderTest()
+        {
+            var provider = new PlayerDataProvider();
+            provider.Initialize();
+        }
+    }
+}

079bfb64727dba3a33841095a372a425cf6f9eac
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-08-03 13:14:34 -0500

    Figuring out the interface ISongDownloadManager

--- SongDownloadManager/ISongDownloadManager.cs
+++ SongDownloadManager/ISongDownloadManager.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace SongDownloadManager
 {
     public interface ISongDownloadManager
     {
+        Dictionary<string, ISongDownloadTarget> DownloadTargets { get; }
+
+        /// <summary>
+        /// Attempts to add an ISongDownloadTarget to the DownloadTargets Dictionary.
+        /// </summary>
+        /// <param name="target"></param>
+        void RegisterDownloadTarget(ISongDownloadTarget target);
+        
+        void DownloadSong(string songIdentifier, IEnumerable<ISongDownloadTarget> targets, Action<int> Progress, CancellationToken cancellationToken);
+        void DownloadSong(string songIdentifier, IEnumerable<ISongDownloadTarget> targets, Action<int> Progress);
+        void DownloadSong(string songIdentifier, IEnumerable<ISongDownloadTarget> targets, CancellationToken cancellationToken);
+        void DownloadSong(string songIdentifier, IEnumerable<ISongDownloadTarget> targets);
+       
     }
 }

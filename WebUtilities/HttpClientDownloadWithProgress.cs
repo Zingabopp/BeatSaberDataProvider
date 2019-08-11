0bc4f11f026d198799bd0fad596119df4b30ce66
Zingabopp <zingabopp@gmail.com>
2019-08-11 13:46:44 -0500

    Added report rate option for HttpClientDownloadWithProgress

--- WebUtilities/HttpClientDownloadWithProgress.cs
+++ WebUtilities/HttpClientDownloadWithProgress.cs
@@ -14,6 +14,7 @@ namespace WebUtilities
     {
         private readonly Uri _downloadUri;
         private readonly string _destinationFilePath;
+        private readonly int ReportRate;
 
         private IWebClient _httpClient;
         public IWebClient WebClient
@@ -29,8 +30,9 @@ namespace WebUtilities
 
         public event ProgressChangedHandler ProgressChanged;
 
-        public HttpClientDownloadWithProgress(IWebClient client, Uri downloadUri, string destinationFilePath)
+        public HttpClientDownloadWithProgress(IWebClient client, Uri downloadUri, string destinationFilePath, int reportRate = 50)
         {
+            ReportRate = Math.Min(reportRate, 1);
             if (client == null)
                 throw new ArgumentNullException(nameof(client), "client cannot be null for HttpClientDownloadWithProgress.");
             if (string.IsNullOrEmpty(destinationFilePath?.Trim()))
@@ -40,8 +42,9 @@ namespace WebUtilities
             _destinationFilePath = destinationFilePath;
         }
 
-        public HttpClientDownloadWithProgress(IWebClient client, string downloadUrl, string destinationFilePath)
+        public HttpClientDownloadWithProgress(IWebClient client, string downloadUrl, string destinationFilePath, int reportRate = 50)
         {
+            ReportRate = Math.Min(reportRate, 1);
             if (string.IsNullOrEmpty(destinationFilePath?.Trim()))
                 throw new ArgumentNullException(nameof(destinationFilePath), "destinationFilePath cannot be null or empty for HttpClientDownloadWithProgress.");
             if (string.IsNullOrEmpty(downloadUrl?.Trim()))
@@ -95,7 +98,7 @@ namespace WebUtilities
                     totalBytesRead += bytesRead;
                     readCount += 1;
 
-                    if (readCount % 100 == 0)
+                    if (ReportRate == 1 || readCount % ReportRate == 0)
                         TriggerProgressChanged(totalDownloadSize, totalBytesRead);
                 }
                 while (isMoreToRead);

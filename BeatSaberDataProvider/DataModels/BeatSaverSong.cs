05d3492d826e18774790666b5485fda788287599
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-08-12 02:35:03 -0500

    Fixed more old code (probably)

--- BeatSaberDataProvider/DataModels/BeatSaverSong.cs
+++ BeatSaberDataProvider/DataModels/BeatSaverSong.cs
@@ -56,58 +56,9 @@ namespace BeatSaberDataProvider.DataModels
             }
         }
 
-        /// <summary>
-        /// Downloads the page and returns it as a string.
-        /// </summary>
-        /// <param name="url"></param>
-        /// <exception cref="HttpRequestException"></exception>
-        /// <returns></returns>
-        public static bool PopulateFields(BeatSaverSong song)
-        {
-            if (song.Populated)
-                return true;
-            bool successful = true;
-            SongGetMethod searchMethod;
-            string url;
-            if (!string.IsNullOrEmpty(song.key))
-            {
-                url = SONG_DETAILS_URL_BASE + song.key;
-                searchMethod = SongGetMethod.SongIndex;
-            }
-            else if (!string.IsNullOrEmpty(song.hash))
-            {
-                url = SONG_BY_HASH_URL_BASE + song.hash;
-                searchMethod = SongGetMethod.Hash;
-            }
-            else
-                return false;
-            Task<string> pageReadTask;
-            //lock (lockObject)
-            pageReadTask = WebUtils.HttpClient.GetStringAsync(url);
-            pageReadTask.Wait();
-            string pageText = pageReadTask.Result;
-            JObject result = new JObject();
-            try
-            {
-                result = JObject.Parse(pageText);
-            }
-            catch (Exception ex)
-            {
-                successful = false;
-                Logger.Exception("Unable to parse JSON from text", ex);
-            }
-            if (searchMethod == SongGetMethod.SongIndex)
-                JsonConvert.PopulateObject(result["song"].ToString(), song);
-            else if (searchMethod == SongGetMethod.Hash)
-                JsonConvert.PopulateObject(result["songs"].First.ToString(), song);
-
-            song.Populated = successful;
-            return successful;
-        }
-
         public bool PopulateFields()
         {
-            return BeatSaverSong.PopulateFields(this);
+            return PopulateFieldsAsync().Result;
         }
 
         enum SongGetMethod
@@ -121,15 +72,15 @@ namespace BeatSaberDataProvider.DataModels
             if (Populated)
                 return true;
             SongGetMethod searchMethod;
-            string url;
+            Uri uri;
             if (!string.IsNullOrEmpty(key))
             {
-                url = SONG_DETAILS_URL_BASE + key;
+                uri = new Uri(SONG_DETAILS_URL_BASE + key);
                 searchMethod = SongGetMethod.SongIndex;
             }
             else if (!string.IsNullOrEmpty(hash))
             {
-                url = SONG_BY_HASH_URL_BASE + hash;
+                uri = new Uri(SONG_BY_HASH_URL_BASE + hash);
                 searchMethod = SongGetMethod.Hash;
             }
             else
@@ -140,7 +91,8 @@ namespace BeatSaberDataProvider.DataModels
             string pageText = "";
             try
             {
-                pageText = await WebUtils.HttpClient.GetStringAsync(url).ConfigureAwait(false);
+                using (var response = await WebUtils.GetBeatSaverAsync(uri).ConfigureAwait(false))
+                    pageText = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             }
             catch (TaskCanceledException)
             {
@@ -224,64 +176,6 @@ namespace BeatSaberDataProvider.DataModels
             }
             return successful;
         }
-        /*
-        public SongInfo ToSongInfo()
-        {
-            if (!Populated)
-            {
-                Logger.Warning("Trying to create SongInfo from an unpopulated SongInfoEnhanced");
-                return null;
-            }
-            if (_songInfo == null)
-            {
-                if (_beatSaverRegex.IsMatch(key))
-                    _songInfo = ScrapedDataProvider.GetSongByKey(key, false);
-                if (_songInfo == null)
-                    _songInfo = ScrapedDataProvider.GetSongByHash(hashMd5, false);
-
-                if (_songInfo == null)
-                {
-                    Logger.Info($"Couldn't find song {key} - {name} by {authorName}, generating new song info...");
-                    _songInfo = new SongInfo() {
-                        key = key,
-                        songName = songName,
-                        songSubName = songSubName,
-                        authorName = authorName,
-                        bpm = bpm,
-                        playedCount = playedCount,
-                        upVotes = upVotes,
-                        downVotes = downVotes,
-                        hash = hashMd5,
-                    };
-                    _songInfo.EnhancedInfo = this;
-                    ScrapedDataProvider.TryAddToScrapedData(_songInfo);
-                    return _songInfo;
-                }
-                _songInfo.EnhancedInfo = this;
-            }
-            return _songInfo;
-        }
-        */
-        //[Obsolete("Shouldn't use this anymore")]
-        //public Song GenerateSongInfo()
-        //{
-        //    var newSong = new Song(hash);
-        //    /*
-        //    var newSong = new SongInfo() {
-        //        key = key,
-        //        songName = metadata.songName,
-        //        songSubName = metadata.songSubName,
-        //        authorName = metadata.levelAuthorName,
-        //        bpm = metadata.bpm,
-        //        playedCount = stats.plays,
-        //        upVotes = stats.upVotes,
-        //        downVotes = stats.downVotes,
-        //        hash = hash,
-        //    };
-        //    */
-        //    //newSong.EnhancedInfo = this;
-        //    return newSong;
-        //}
 
         public override string ToString()
         {
@@ -296,6 +190,8 @@ namespace BeatSaberDataProvider.DataModels
 
         public bool Equals(BeatSaverSong other)
         {
+            if (other == null)
+                return false;
             return hash.ToUpper() == other.hash.ToUpper();
         }
 
@@ -308,22 +204,7 @@ namespace BeatSaberDataProvider.DataModels
                 return match.Success ? int.Parse(match.Groups[1].Value, System.Globalization.NumberStyles.HexNumber) : 0;
             }
         }
-        /*
-        [JsonProperty("id")]
-        private int _id;
-        [JsonIgnore]
-        public int id
-        {
-            get
-            {
-                if (!(_id > 0))
-                    if (key != null && _beatSaverRegex.IsMatch(key))
-                        _id = int.Parse(key.Substring(0, key.IndexOf('-')));
-                return _id;
-            }
-            set { _id = value; }
-        }
-        */
+
         [JsonProperty("metadata")]
         public SongMetadata metadata { get; }
         [JsonProperty("stats")]
@@ -458,14 +339,7 @@ namespace BeatSaberDataProvider.DataModels
     {
         public static void PopulateFromBeatSaver(this IEnumerable<BeatSaverSong> songs)
         {
-            List<Task> populateTasks = new List<Task>();
-            for (int i = 0; i < songs.Count(); i++)
-            {
-                if (!songs.ElementAt(i).Populated)
-                    populateTasks.Add(songs.ElementAt(i).PopulateFieldsAsync());
-            }
-
-            Task.WaitAll(populateTasks.ToArray());
+            songs.PopulateFromBeatSaverAsync().Wait();
         }
 
         public static async Task PopulateFromBeatSaverAsync(this IEnumerable<BeatSaverSong> songs)
@@ -478,7 +352,6 @@ namespace BeatSaberDataProvider.DataModels
             }
 
             await Task.WhenAll(populateTasks).ConfigureAwait(false);
-            Logger.Warning("Finished PopulateAsync?");
         }
     }
 

4ee129f004ef1fcf89460df0caa06e709f0e25f0
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-08-11 22:29:49 -0500

    Cleaned up SongFeedReaders.WebUtils

--- SongFeedReaders/WebUtils.cs
+++ SongFeedReaders/WebUtils.cs
@@ -17,22 +17,6 @@ namespace SongFeedReaders
         public static FeedReaderLoggerBase Logger { get { return _logger; } set { _logger = value; } }
         public static bool IsInitialized { get; private set; }
         private static readonly TimeSpan RateLimitPadding = new TimeSpan(0, 0, 0, 0, 100);
-        //private static readonly object lockObject = new object();
-        //private static HttpClientHandler _httpClientHandler;
-        //public static HttpClientHandler HttpClientHandler
-        //{
-        //    get
-        //    {
-        //        if (_httpClientHandler == null)
-        //        {
-        //            _httpClientHandler = new HttpClientHandler();
-        //            HttpClientHandler.MaxConnectionsPerServer = 10;
-        //            HttpClientHandler.UseCookies = true;
-        //            HttpClientHandler.AllowAutoRedirect = true; // Needs to be false to detect Beat Saver song download rate limit
-        //        }
-        //        return _httpClientHandler;
-        //    }
-        //}
 
         private static IWebClient _webClient;
         public static IWebClient WebClient
@@ -59,7 +43,7 @@ namespace SongFeedReaders
         /// <summary>
         /// Maybe have to move to WebUtils and use url.LastIndexOf("/")
         /// </summary>
-        public static ConcurrentDictionary<string, DateTime> WaitForRateLimitDict = new ConcurrentDictionary<string, DateTime>();
+        private static ConcurrentDictionary<string, DateTime> WaitForRateLimitDict = new ConcurrentDictionary<string, DateTime>();
 
         public static async Task WaitForRateLimit(string baseUrl)
         {
@@ -74,12 +58,20 @@ namespace SongFeedReaders
             return;
         }
 
+        /// <summary>
+        /// Use to get web responses from Beat Saver. If the rate limit is reached, it waits for the limit to expire before trying again.
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="retries"></param>
+        /// <returns></returns>
         public static async Task<IWebResponseMessage> GetBeatSaverAsync(Uri uri, int retries = 5)
         {
+            
             bool rateLimitExceeded = false;
             int tries = 0;
             IWebResponseMessage response;
-            string baseUrl = uri.OriginalString.Substring(0, uri.OriginalString.LastIndexOf("/"));
+            string baseUrl = uri?.OriginalString.Substring(0, uri.OriginalString.LastIndexOf("/")) 
+                ?? throw new ArgumentNullException(nameof(uri), "uri cannot be null for WebUtils.GetBeatSaverAsync");
             await WaitForRateLimit(baseUrl).ConfigureAwait(false); // Wait for an existing rate limit if it exists
             do
             {
@@ -121,6 +113,11 @@ namespace SongFeedReaders
             return response;
         }
 
+        /// <summary>
+        /// Converts a Unix timestamp to a DateTime object.
+        /// </summary>
+        /// <param name="unixTimeStamp"></param>
+        /// <returns></returns>
         public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
             // Unix timestamp is seconds past epoch
@@ -172,6 +169,10 @@ namespace SongFeedReaders
                 return null;
         }
 
+        /// <summary>
+        /// Initializes WebUtils, this class cannot be used before calling this.
+        /// </summary>
+        /// <param name="client"></param>
         public static void Initialize(IWebClient client)
         {
             if (client == null)
@@ -192,85 +193,5 @@ namespace SongFeedReaders
         public int CallsRemaining { get; set; }
         public DateTime TimeToReset { get; set; }
         public int CallsPerReset { get; set; }
-    }
-
-    //public class HttpGetException : Exception
-    //{
-    //    public HttpStatusCode HttpStatusCode { get; private set; }
-    //    public string Url { get; private set; }
-
-    //    public HttpGetException()
-    //        : base()
-    //    {
-    //        base.Data.Add("StatusCode", HttpStatusCode.BadRequest);
-    //        base.Data.Add("Url", string.Empty);
-    //    }
-
-    //    public HttpGetException(string message)
-    //        : base(message)
-    //    {
-
-    //        base.Data.Add("StatusCode", HttpStatusCode.BadRequest);
-    //        base.Data.Add("Url", string.Empty);
-    //    }
-
-    //    public HttpGetException(string message, Exception inner)
-    //        : base(message, inner)
-    //    {
-    //        base.Data.Add("StatusCode", HttpStatusCode.BadRequest);
-    //        base.Data.Add("Url", string.Empty);
-    //    }
-
-    //    public HttpGetException(HttpStatusCode code, string url)
-    //        : base()
-    //    {
-    //        base.Data.Add("StatusCode", code);
-    //        base.Data.Add("Url", url);
-    //        HttpStatusCode = code;
-    //        Url = url;
-    //    }
-
-    //    public HttpGetException(HttpStatusCode code, string url, string message)
-    //    : base(message)
-    //    {
-    //        base.Data.Add("StatusCode", code);
-    //        base.Data.Add("Url", url);
-    //        HttpStatusCode = code;
-    //        Url = url;
-    //    }
-    //}
-
-    // From https://stackoverflow.com/questions/45711428/download-file-with-webclient-or-httpclient
-    public static class HttpContentExtensions
-    {
-        /// <summary>
-        /// Downloads the provided HttpContent to the specified file.
-        /// </summary>
-        /// <param name="content"></param>
-        /// <param name="filename"></param>
-        /// <param name="overwrite"></param>
-        /// <exception cref="ArgumentNullException">Thrown when content or the filename are null or empty.</exception>
-        /// <exception cref="InvalidOperationException">Thrown when overwrite is false and a file at the provided path already exists.</exception>
-        /// <returns></returns>
-        public static async Task ReadAsFileAsync(this IWebResponseContent content, string filename, bool overwrite)
-        {
-            if (content == null)
-                throw new ArgumentNullException(nameof(content), "content cannot be null for HttpContent.ReadAsFileAsync");
-            if (string.IsNullOrEmpty(filename?.Trim()))
-                throw new ArgumentNullException(nameof(filename), "filename cannot be null or empty for HttpContent.ReadAsFileAsync");
-            string pathname = Path.GetFullPath(filename);
-            if (!overwrite && File.Exists(filename))
-            {
-                throw new InvalidOperationException(string.Format("File {0} already exists.", pathname));
-            }
-
-            using (Stream contentStream = await content.ReadAsStreamAsync().ConfigureAwait(false))
-            {
-                using (Stream writeStream = File.Open(filename, FileMode.Create, FileAccess.Write, FileShare.None))
-                {
-                    await contentStream.CopyToAsync(writeStream).ConfigureAwait(false);
-                }
-            }
-        }
     }
 }

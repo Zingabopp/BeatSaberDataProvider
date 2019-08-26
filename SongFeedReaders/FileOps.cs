f5777df96beaaa6e732389127bd1e5b136c4dd08
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-08-26 16:45:18 -0500

    Remove zip stuff

--- SongFeedReaders/FileOps.cs
+++ SongFeedReaders/FileOps.cs
@@ -1,9 +1,6 @@
 ﻿using SongFeedReaders.Logging;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
-using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -31,61 +28,6 @@ namespace SongFeedReaders
                 actualPath = await response.Content.ReadAsFileAsync(path, overwrite).ConfigureAwait(false);
             }
             return actualPath;
-        }
-
-        /// <summary>
-        /// Extracts a zip file to the specified directory.
-        /// </summary>
-        /// <param name="zipPath">Path to zip file</param>
-        /// <param name="extractDirectory">Directory to extract to</param>
-        /// <param name="deleteZip">If true, deletes zip file after extraction</param>
-        /// <param name="overwriteTarget">If true, overwrites existing files with the zip's contents</param>
-        /// <returns></returns>
-        public static async Task<string[]> ExtractZipAsync(string zipPath, string extractDirectory, bool deleteZip = true, bool overwriteTarget = true)
-        {
-            if (string.IsNullOrEmpty(zipPath))
-                throw new ArgumentNullException(nameof(zipPath));
-            if (string.IsNullOrEmpty(extractDirectory))
-                throw new ArgumentNullException(nameof(extractDirectory));
-
-            FileInfo zipFile = new FileInfo(zipPath);
-            DirectoryInfo extDir = new DirectoryInfo(extractDirectory);
-            if (!zipFile.Exists)
-                throw new ArgumentException($"File at zipPath {zipFile.FullName} does not exist.", nameof(zipPath));
-            extDir.Create();
-            //var extractedFiles = await ExtractAsync(zipFile.FullName, extDir.FullName, overwriteTarget).ConfigureAwait(true);
-            string[] extractedFiles = null;
-            if (await Utilities.WaitUntil(() =>
-            {
-                try
-                {
-                    using (ZipArchive zipArchive = ZipFile.OpenRead(zipPath))
-                        zipArchive.ExtractToDirectory(extDir.FullName);
-                    return true;
-#pragma warning disable CA1031 // Do not catch general exception types
-                }
-                catch (Exception)
-#pragma warning restore CA1031 // Do not catch general exception types
-                {
-                    return false;
-                }
-
-            }, 25, 3000).ConfigureAwait(false))
-            {
-
-            }
-            if (deleteZip)
-            {
-                try
-                {
-                    var deleteSuccessful = await TryDeleteAsync(zipFile.FullName).ConfigureAwait(false);
-                }
-                catch (IOException ex)
-                {
-                    Logger.Warning($"Unable to delete file {zipFile.FullName}.\n{ex.Message}\n{ex.StackTrace}");
-                }
-            }
-            return extractedFiles;
         }
 
         public static string GetSafeDirectoryPath(string directory)

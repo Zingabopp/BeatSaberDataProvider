1dda23086da719842848b340a6c9760401db9993
Zingabopp <Zingabopp@gmail.com>
2019-12-25 22:49:27 -0600

    BuiltInFilters gracefully handle properties that don't exist.

--- SongFeedReaders/Filtering/BuiltInFilters.cs
+++ SongFeedReaders/Filtering/BuiltInFilters.cs
@@ -11,26 +11,26 @@ namespace SongFeedReaders.Filtering
     {
         public static Func<ScrapedSong, bool> ThreeSixtyDegree => new Func<ScrapedSong, bool>(song =>
         {
-            var meta = song.JsonData["metadata"];
-            var chara = meta["characteristics"];
-            var list = chara.Children().ToList();
-            return chara.Any(t => t["name"].Value<string>() == "360Degree");
+            var meta = song?.JsonData["metadata"];
+            var chara = meta?["characteristics"];
+            if (chara == null) return false;
+            return chara.Any(t => (t["name"]?.Value<string>() ?? string.Empty) == "360Degree");
         });
         
         public static Func<ScrapedSong, bool> NinetyDegree => new Func<ScrapedSong, bool>(song =>
         {
-            var meta = song.JsonData["metadata"];
-            var chara = meta["characteristics"];
-            var list = chara.Children().ToList();
-            return chara.Any(t => t["name"].Value<string>() == "90Degree");
+            var meta = song?.JsonData["metadata"];
+            var chara = meta?["characteristics"];
+            if (chara == null) return false;
+            return chara.Any(t => (t["name"]?.Value<string>() ?? string.Empty) == "90Degree");
         });
 
         public static Func<ScrapedSong, bool> OneSaber => new Func<ScrapedSong, bool>(song =>
         {
-            var meta = song.JsonData["metadata"];
-            var chara = meta["characteristics"];
-            var list = chara.Children().ToList();
-            return chara.Any(t => t["name"].Value<string>() == "OneSaber");
+            var meta = song?.JsonData["metadata"];
+            var chara = meta?["characteristics"];
+            if (chara == null) return false;
+            return chara.Any(t => (t["name"]?.Value<string>() ?? string.Empty) == "OneSaber");
         });
 
 

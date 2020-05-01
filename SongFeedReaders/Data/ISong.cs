fe858094ddf6da58585a9b8505f25a15e198390a
Zingabopp <Zingabopp@gmail.com>
2020-05-01 12:39:55 -0500

    ISong.Hash non-nullable

--- SongFeedReaders/Data/ISong.cs
+++ SongFeedReaders/Data/ISong.cs
@@ -9,7 +9,7 @@ namespace SongFeedReaders.Data
         /// <summary>
         /// Beat Saver hash of the song, always uppercase.
         /// </summary>
-        string? Hash { get; set; }
+        string Hash { get; set; }
         string? Name { get; set; }
         string? Key { get; set; }
         string? LevelAuthorName { get; set; }
